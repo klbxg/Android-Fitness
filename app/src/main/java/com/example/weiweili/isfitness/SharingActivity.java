d6eae87151b592d1ffe3f7457a304c95c5a2e502
klbxg <liweiwei0404@gmail.com>
2015-09-06 15:49:55 -0700

    share activity

--- app/src/main/java/com/example/weiweili/isfitness/SharingActivity.java
+++ app/src/main/java/com/example/weiweili/isfitness/SharingActivity.java
@@ -1,16 +1,60 @@
 package com.example.weiweili.isfitness;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class SharingActivity extends AppCompatActivity {
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
+public class SharingActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String SERVER_ADDRESS = "http://isfitness.site50.net/";
+
+    Button bSportUploadImage;
+    EditText etSportUploadImageName;
+    ImageView ivSportToUpload;
+    UserLocalStore userLocalStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sharing);
+
+        etSportUploadImageName = (EditText) findViewById(R.id.etUploadName);
+
+        ivSportToUpload = (ImageView) findViewById(R.id.imageToUpload);
+
+        bSportUploadImage = (Button) findViewById(R.id.bUploadImage);
+
+        bSportUploadImage.setOnClickListener(this);
+        userLocalStore = new UserLocalStore(this);
+        Intent intent = this.getIntent();
+        Bitmap bitmap = intent.getParcelableExtra("sportResult");
+        ivSportToUpload.setImageBitmap(bitmap);
     }
 
     @Override
@@ -33,5 +77,65 @@ public class SharingActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bActivityUploadImage:
+                User user = userLocalStore.getLoggedInUser();
+                Bitmap image = ((BitmapDrawable) ivSportToUpload.getDrawable()).getBitmap();
+                new UploadImage(image, etSportUploadImageName.getText().toString(), user.username).execute();
+                Intent intent1 = new Intent(SharingActivity.this, MyPage.class);
+                startActivity(intent1);
+                finish();
+                break;
+        }
+    }
+
+    private class UploadImage extends AsyncTask<Void, Void, Void> {
+        Bitmap image;
+        String name;
+        String username;
+        public UploadImage(Bitmap image, String name, String username) {
+            this.image = image;
+            this.name = name;
+            this.username = username;
+        }
+        @Override
+        protected Void doInBackground(Void... params) {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            image.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+            String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
+            ArrayList<NameValuePair> dataToSend = new ArrayList<>();
+            dataToSend.add(new BasicNameValuePair("image", encodedImage));
+            dataToSend.add(new BasicNameValuePair("name", name));
+            dataToSend.add(new BasicNameValuePair("username", username));
+
+            HttpParams httpRequestParams = getHttpRequestParams();
+            HttpClient client = new DefaultHttpClient(httpRequestParams);
+            HttpPost post = new HttpPost(SERVER_ADDRESS + "SavePictures.php");
+
+            try {
+                post.setEntity(new UrlEncodedFormEntity(dataToSend));
+                client.execute(post);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Toast.makeText(getApplicationContext(), "Activity Uploaded", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private HttpParams getHttpRequestParams() {
+        HttpParams httpRequestParams = new BasicHttpParams();
+        HttpConnectionParams.setConnectionTimeout(httpRequestParams, 1000 * 30);
+        HttpConnectionParams.setSoTimeout(httpRequestParams, 1000 * 30);
+        return httpRequestParams;
     }
 }
